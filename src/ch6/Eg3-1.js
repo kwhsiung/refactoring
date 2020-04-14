24f2ef677e964c5dc39c45548d12940d6d862baf
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-04-14 20:12:36 +0800

    feat: add ch6 eg2 and eg3

---
+++ src/ch6/Eg3-1.js
@@ -0,0 +1,32 @@
+function printOwing(invoice) {
+  printBanner()
+
+  // calculate outstanding
+  let outstanding = 0
+  for (const o of invoice.orders) {
+    outstanding += o.amount
+  }
+  recordDueDate(invoice)
+  printDetails(invoice, outstanding)
+}
+
+function printBanner() {
+  console.log('***********************')
+  console.log('**** Customer Owes ****')
+  console.log('***********************')
+}
+
+function printDetails(invoice, outstanding) {
+  console.log(`name: ${invoice.customer}`)
+  console.log(`amount: ${outstanding}`)
+  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`)
+}
+
+function recordDueDate(invoice) {
+  const today = Clock.today
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  )
+}
